ed576193a30aab352a407f459bd40571e0a316fd
He Qi <qihen@amazon.com>
2023-09-01 17:25:16 +0800

    webpack proxy to api

--- web/vue.config.js
+++ web/vue.config.js
@@ -38,7 +38,14 @@ module.exports = {
   lintOnSave: true,
   devServer: {
     publicPath, // 和 publicPath 保持一致
-    disableHostCheck: process.env.NODE_ENV === 'development' // 关闭 host check，方便使用 ngrok 之类的内网转发工具
+    allowedHosts: ['all'],
+    disableHostCheck: process.env.NODE_ENV === 'development', // 关闭 host check，方便使用 ngrok 之类的内网转发工具
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000'
+        // changeOrigin: true
+      }
+    }
   },
   css: {
     loaderOptions: {
