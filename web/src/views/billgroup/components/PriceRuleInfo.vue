d399598a40dac722485fa27476e883643638a095
He Qi <qihen@amazon.com>
2023-11-03 01:24:21 +0800

    dialog for price plan details

--- web/src/views/billgroup/components/PriceRuleInfo.vue
+++ web/src/views/billgroup/components/PriceRuleInfo.vue
@@ -1,43 +1,94 @@
 <template>
-  <el-dialog title="折扣详情" :visible.sync="visible" v-loading="!isShow">
-    <el-table :data="gridData">
-      <el-table-column property="date" label="日期" width="150"></el-table-column>
-      <el-table-column property="name" label="姓名" width="200"></el-table-column>
-      <el-table-column property="address" label="地址"></el-table-column>
-    </el-table>
-  </el-dialog>
+  <div>
+    <el-dialog v-bind="$attrs" v-on="$listeners" @open="onOpen" @close="onClose" title="Price Rule Details" :visible.sync="visible">
+      <el-row :gutter="15">
+        <el-form ref="elForm" :model="formData" :rules="rules" size="medium" label-width="100px">
+          <el-col :span="13">
+            <el-form-item label="Scope" prop="scope">
+              <el-input disable v-model="formData.scope" placeholder="请输入Scope" prefix-icon='el-icon-monitor'
+                        :style="{width: '100%'}"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="13">
+            <el-form-item label="Rate" prop="rate">
+              <el-input v-model="formData.rate" placeholder="请输入Rate" :style="{width: '100%'}"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-form>
+      </el-row>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
+import { CreateCustomLineItem, GetPriceRuleDetails } from '@/views/billgroup/api'
+
 export default {
   name: 'PriceRuleInfo',
+  inheritAttrs: false,
   components: {},
   props: {
-    arn: {
-      type: String,
-      default: ''
-    },
-    visible: {
-      type: Boolean,
-      default: false
-    }
+    // visible: {
+    //   type: Boolean,
+    //   default: false
+    // }
   },
   data () {
     return {
-      gridData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }],
-      pRule: {},
-      isShow: false
+      visible: false,
+      formData: {
+        scope: 'Global',
+        rate: '0%'
+      },
+      rules: {
+        scope: [{
+          required: true,
+          message: '请输入Scope',
+          trigger: 'blur'
+        }],
+        rate: [{
+          required: true,
+          message: '请输入Rate',
+          trigger: 'blur'
+        }],
+      },
+    }
+  },
+  computed: {},
+  created () {
+  },
+  mounted () {
+  },
+  methods: {
+    async showDialog (arn) {
+      console.log(arn)
+      this.visible = true
+      try {
+        const res = await GetPriceRuleDetails({ Arn: arn }, { account_id: this.$store.state.d2admin.billgroup.accountDetails.account_id })
+        console.log(JSON.stringify(res))
+        this.formData.scope = res.data.PricingRules[0].Scope
+        this.formData.rate = res.data.PricingRules[0].Type + ' - ' + res.data.PricingRules[0].ModifierPercentage + ' %'
+      } catch (e) {
+        console.log(e)
+      } finally {
+      }
+    },
+    onOpen () {
+      console.log('open priceRule Dialog')
+    },
+    onClose () {
+      console.log('onClose priceRule Dialog')
+      this.$refs['elForm'].resetFields()
+    },
+    close () {
+      console.log('close dialog')
+      this.$emit('update:visible', false)
     }
   },
   watch: {
     arn (newVal, oldVal) {
       console.log(`新值：${newVal}`)
       console.log(`旧值：${oldVal}`)
-      console.log('hellow  world')
     }
   }
 }
